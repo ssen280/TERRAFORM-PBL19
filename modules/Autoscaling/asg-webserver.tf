983522d7f2bf639d9bae1e87906f52571d18ffb4
ssen280 <105562242+ssen280@users.noreply.github.com>
2022-08-29 18:30:37 +0530

    Update asg-webserver.tf

--- modules/Autoscaling/asg-webserver.tf
+++ modules/Autoscaling/asg-webserver.tf
@@ -26,7 +26,7 @@ resource "aws_autoscaling_group" "wordpress-asg" {
 # # attaching autoscaling group of  wordpress application to internal loadbalancer
 resource "aws_autoscaling_attachment" "asg_attachment_wordpress" {
   autoscaling_group_name = aws_autoscaling_group.wordpress-asg.id
-  alb_target_group_arn   = var.wordpress-alb-tgt
+  lb_target_group_arn   = var.wordpress-alb-tgt
 }
 
 
