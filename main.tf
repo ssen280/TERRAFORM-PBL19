a3d1b8b452b9355689ee242d44ad2c8b77d2608a
ssen280 <105562242+ssen280@users.noreply.github.com>
2022-08-29 18:22:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -112,9 +112,9 @@ module "RDS" {
 # The Module creates instances for jenkins, sonarqube abd jfrog
 module "compute" {
   source          = "./modules/compute"
-  ami-jenkins     = var.ami
-  ami-sonar       = var.ami
-  ami-jfrog       = var.ami
+  ami-jenkins     = var.ami-bastion
+  ami-sonar       = var.ami-sonar
+  ami-jfrog       = var.ami-bastion
   subnets-compute = module.VPC.public_subnets-1
   sg-compute      = [module.security.ALB-sg]
   keypair         = var.keypair
