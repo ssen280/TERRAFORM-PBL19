431a7ece9b1b00e55c5da86088bee2da98651fa1
ssen280 <105562242+ssen280@users.noreply.github.com>
2022-08-29 18:45:00 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region = var.region
-  access_key = "AKIAXQGDGTKLQWAGYGTB"
-  secret_key = "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"
+  #access_key = "AKIAXQGDGTKLQWAGYGTB"
+  #secret_key = "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"
 }
