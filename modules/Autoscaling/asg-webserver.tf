916804ae1cdc682178abe202022ac4c43af0944b
ssen280 <105562242+ssen280@users.noreply.github.com>
2022-08-29 18:36:26 +0530

    Update asg-webserver.tf

--- modules/Autoscaling/asg-webserver.tf
+++ modules/Autoscaling/asg-webserver.tf
@@ -57,5 +57,5 @@ resource "aws_autoscaling_group" "tooling-asg" {
 # # # attaching autoscaling group of  tooling application to internal loadbalancer
 resource "aws_autoscaling_attachment" "asg_attachment_tooling" {
   autoscaling_group_name = aws_autoscaling_group.tooling-asg.id
-  alb_target_group_arn   = var.tooling-alb-tgt
+  lb_target_group_arn   = var.tooling-alb-tgt
 }
