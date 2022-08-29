2ea6cfd9ffe78a2d84182db92460853f999fa1c7
ssen280 <sensaikat280@gmail.com>
2022-08-29 16:59:23 +0530

    updated provider file

--- providers.tf
+++ providers.tf
@@ -1,10 +1,11 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
+      required_providers {
+         aws = {
+         source = "hashicorp/aws"
+         version = "= 3.74.2"
+        }
+     }
   }
-}
 
 provider "aws" {
   region = var.region
